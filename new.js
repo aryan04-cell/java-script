2437dea7c779bc0809fa45db94fad01a1b4199cd
Aryan Choudhary <choudharyatat08@gmail.com>
2025-06-10 18:59:53 +0000

    notes

--- new.js
+++ new.js
@@ -1,1 +1,41 @@
-console.log("aryan")
+/*console.log("aryan")
+ let accountId ="789o9"
+ const accountName ="rsifdznb f"
+
+ accountId ="67"
+ 
+
+ console.table({accountId , accountName})
+  
+ console.log(8+9)
+//  code readability should be high 
+// documentation tc39.es and mdn
+
+
+console.log("hm h aryann")*/
+// console.log(typeof null) //null ka type object aata h , and undefined ka type undifined hi aata h 
+// to convert some variable into other variable 
+let age = "20"
+
+console.log(typeof age)
+
+let true_age =Number(age) // to convert into other first letter nof the data type must be capital 
+
+console.log(typeof true_age)
+
+/* convertion of data types into Number
+"33" = 33
+"33abc" = NaN(not a number ) but its type is a number 
+"false" = 0
+null = 0
+undefined = NaN
+*/
+
+
+/* convertion of data types into Boolean
+ 1= true
+ 0= false
+ "aryhan" = true
+ "" = false
+ 
+*/
